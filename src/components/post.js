9a95dc5706af0526ce6d5cbff36aefe4adfd8367
zhuganglie <iwifer@gmail.com>
2021-03-11 14:25:07 +0800

    add disqus

--- src/components/post.js
+++ src/components/post.js
@@ -1,24 +1,19 @@
-import React, {useEffect} from "react"
+import * as React from "react"
 import {Link, graphql } from "gatsby"
 import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import Layout from "../components/layout"
 import { kebabCase } from "lodash"
-import commentBox from "commentbox.io"
+import { DiscussionEmbed } from "disqus-react"
 import {AiOutlineTag} from "react-icons/ai"
 import {FiCalendar} from "react-icons/fi"
 
 const shortcodes = { Link } // Provide common components here
-
+const disqusConfig = {
+  shortname: process.env.GATSBY_DISQUS_NAME,
+  config: { identifier: `{mdx.frontmatter.title}`, },
+}
 export default function PageTemplate({ data: { mdx } }) {
-  useEffect(() =>{
-    commentBox('5750314422501376-proj', {
-      backgroundColor: 'transparent',
-      textColor: 'black',
-      subtextColor: 'grey',
-    }
-    )
-  },[])
   return (
     <Layout>
     <div>
@@ -34,7 +29,7 @@ export default function PageTemplate({ data: { mdx } }) {
         <span className="text-red-900 font-bold"><Link to="/tags">标签：</Link></span>
          {mdx.frontmatter.tags.map(tag => (<Link to={`/tags/${kebabCase(tag)}/`} className="flex text-red-900 px-1"><AiOutlineTag size="18" color="#7F1D1D" />{tag}</Link>))}
         </div>
-    <div className="commentbox" />
+        <DiscussionEmbed {...disqusConfig} />
     </Layout>
   )
 }
